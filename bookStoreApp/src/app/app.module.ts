6fd41d55e0d034bd5ea6346e5c5b1590aba0a82a
Dinesh Gaud <gaudinesh@gmail.com>
2021-10-27 15:19:52 +0530

    bootstrap and angular material added with new modules

--- bookStoreApp/src/app/app.module.ts
+++ bookStoreApp/src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { HowItWorksComponent } from './how-it-works/how-it-works.component';
+import { SharedModule } from './shared/shared.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutUsComponent,
+    HowItWorksComponent    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,    
+    // AuthModule,
+    // UserModule,
+    // PublicModule,
+    SharedModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
